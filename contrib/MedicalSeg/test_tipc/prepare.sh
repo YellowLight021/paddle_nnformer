ac3a4cfbd0a58713259cad8973503118b4fc8fa6
sheyan21@126.com <45915150+YellowLight021@users.noreply.github.com>
2022-08-04 14:38:44 +0800

    paddle_nnformer
    提交

---
+++ contrib/MedicalSeg/test_tipc/prepare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./test_tipc/common_func.sh
+
+FILENAME=$1
+# MODE be one of ['lite_train_lite_infer']
+MODE=$2
+
+dataline=$(cat ${FILENAME})
+
+# parser params
+IFS=$'\n'
+lines=(${dataline})
+
+# The training params
+model_name=$(func_parser_value "${lines[4]}")
+
+trainer_list=$(func_parser_value "${lines[14]}")
+
+# MODE be one of ['lite_train_lite_infer']
+if [ ${MODE} = "lite_train_lite_infer" ];then
+
+    mkdir -p ./test_tipc/data
+    rm -rf ./test_tipc/data/mini_acdc
+    cd ./test_tipc/data/
+    unzip mini_acdc.zip  && cd ../../
+fi
